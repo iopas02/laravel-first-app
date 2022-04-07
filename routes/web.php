dd512dd6118c3da26198d5af6bdf2c7c6b0ddefa
Charles Daniel <abuzo.daniel@gmail.com>
2022-04-08 01:38:21 +0800

    wala

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
-Route::get('/about', [HomeController::class, 'about']);
+Route::get('/about', [HomeController::class, 'about'])->name('home.about');
 
-Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
 
 Route::get('/store/{category?}/{item?}', function($category = null, $item = null){
     if(isset($category)){
